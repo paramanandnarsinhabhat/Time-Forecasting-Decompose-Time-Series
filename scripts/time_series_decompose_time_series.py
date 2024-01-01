da57f51070aead9e99f41c27d6d6190bd7d6c3b7
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-01 19:50:07 +0530

    code changes completed

--- scripts/time_series_decompose_time_series.py
+++ scripts/time_series_decompose_time_series.py
@@ -7,3 +7,19 @@ train['Date'] = pd.to_datetime(train['Date'], format='%Y-%m-%d')
 train.index = train['Date']
 
 print(train.dtypes)
+
+plt.figure(figsize=(12,8))
+
+plt.plot(train.index, train['count'], label='Train')
+plt.legend(loc='best')
+plt.show()
+
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+decomposed_series = seasonal_decompose(train['count'],)
+
+
+decomposed_series.plot()
+plt.show()
+
+(decomposed_series.seasonal[0:30]).plot()
